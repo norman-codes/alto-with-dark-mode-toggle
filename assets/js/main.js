55d54c5daaa7e7809fe0c69ce13768745fe1c60f
norman <nbukingolts@ufl.edu>
2025-02-16 20:08:48 -0500

    Reloads comment section to dynamically update style upon dark mode toggle; increments version

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,33 @@ function darkModeToggle() {
         } else {
             localStorage.setItem('alto_dark', false);
         }
+
+        // Reload iframe element in comments
+        /*
+            Why? Because the comments section in a post, which is
+            rendered as an iframe element, was not updating when
+            the dark mode toggle was applied due to being rendered
+            once per page load with styling from its parent element.
+            
+            See ("auto") in the comments attributes here in the docs:
+            https://ghost.org/docs/themes/helpers/comments/#attributes
+
+            The `ghost-comments-root` id appears to be universal, so
+            this should apply regardless of any custom comment styling.
+
+            Why not all iframe elements? Because that could have negative
+            performance implications in the post page and beyond.
+        */
+            $('#ghost-comments-root iframe').each(function() {
+                // Reference: https://stackoverflow.com/questions/4249809/reload-an-iframe-with-jquery
+                /*
+                    Note that this causes a "Source map error" because, briefly,
+                    the iframe is loading from an invalid URL (as it is effectively
+                    being "reset" by the code below).
+                */
+                var srcdoc = $(this).attr('srcdoc');
+                $(this).attr('srcdoc', srcdoc);
+            });
     });
 }
 
